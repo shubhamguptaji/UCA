5e5b909585b37b9aac6bf3eed3dc9a0ca94403bf
Shubham Gupta <shubham2604gupta@gmail.com>
2018-09-04 22:02:11 +0530

    4/9/2018

---
+++ APC/Algorithms/binary_tree.cpp
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stack>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+typedef struct Btree
+{
+    int data;
+    struct Btree *left;
+    struct Btree *right;
+} Btree;
+
+void insert(struct Btree *temp, int key)
+{
+    Btree *a = new Btree();
+    a->data = key;
+    a->left = NULL;
+    a->right = NULL;
+    if (temp == NULL)
+    {
+        temp = a;
+        return;
+    }
+    queue<struct Btree *> q;
+    q.push(temp);
+    while (!q.empty())
+    {
+        struct Btree *temp = q.front();
+        q.pop();
+
+        if (!temp->left)
+            q.push(temp->left);
+        else
+        {
+            temp->left = a;
+            break;
+        }
+
+        if (!temp->right)
+            q.push(temp->right);
+        else
+        {
+            temp->right = a;
+            break;
+        }
+    }
+}
+
+Btree *putValue(Btree *root, int data)
+{
+    Btree *nn = new Btree();
+    nn->data = data;
+    nn->left = NULL;
+    nn->right = NULL;
+    if (root == NULL)
+        return nn;
+
+    queue<Btree *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Btree *node = q.front();
+        q.pop();
+        if (node->left)
+            q.push(node->left);
+        else
+        {
+            node->left = nn;
+            return root;
+        }
+        if (node->right)
+            q.push(node->right);
+        else
+        {
+            node->right = nn;
+            return root;
+        }
+    }
+}
+
+void inOrder(Btree *root)
+{
+    if (root == NULL)
+        return;
+    inOrder(root->left);
+    printf("%d ", root->data);
+    inOrder(root->right);
+}
+
+void preOrder(Btree *root)
+{
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(Btree *root)
+{
+    if (root == NULL)
+        return;
+    postOrder(root->left);
+    postOrder(root->right);
+    printf("%d ", root->data);
+}
+
+void inOrderIterative(Btree *root)
+{
+    stack<Btree *> stk;
+    while (1)
+    {
+        while (root)
+        {
+            stk.push(root);
+            root = root->left;
+        }
+        if (stk.empty())
+            return;
+        root = stk.top();
+        stk.pop();
+        printf("%d ", root->data);
+        root = root->right;
+    }
+}
+
+void preOrderIterative(Btree *root)
+{
+    stack<Btree *> stk;
+    while (1)
+    {
+        printf("%d  ", root->data);
+        while (root)
+        {
+            stk.push(root);
+            root = root->left;
+        }
+        if (stk.empty())
+            return;
+        root = stk.top();
+        stk.pop();
+        // root = root->right;
+    }
+}
+
+void postOrderIterative(Btree *root)
+{
+}
+
+int main()
+{
+    system("cat z_value.c");
+    Btree *root = NULL;
+    root = putValue(root, 1);
+    root = putValue(root, 2);
+    root = putValue(root, 3);
+    root = putValue(root, 4);
+    root = putValue(root, 5);
+    root = putValue(root, 6);
+    root = putValue(root, 7);
+
+    inOrder(root);
+    cout << endl;
+    preOrder(root);
+    cout << endl;
+    postOrder(root);
+    cout << endl;
+    inOrderIterative(root);
+    cout << endl;
+    // preOrderIterative(root);
+    return 0;
+}
--- APC/InterviewBit/excel_column_title.c
+++ APC/InterviewBit/excel_column_title.c
@@ -1,8 +1,53 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<math.h>
 
-int main() {
-    system("echo hello");
+int main()
+{
+    int n = 943566;
+    // int count = 0, count1 = 0;
+    // char a[3];
+    // while (n > 26)
+    // {
+    //     n -= 26;
+    //     count++;
+    // }
+    // while (count > 26)
+    // {
+    //     count -= 26;
+    //     count1++;
+    // }
+    // if (count1)
+    // {
+    //     a[0] = count1 + 64;
+    //     a[1] = count + 64;
+    //     a[2] = n + 64;
+    // }
+    // else
+    // {
+    //     a[0] = count + 64;
+    //     a[1] = n + 64;
+    // }
+    int i=0;
+    while(n/pow(26,i++)>26);
+    char *a = (char*)malloc(sizeof(char) * (i));
+    printf("%d ", i);
+    i--;
+    int j=0;
+    while(i>0) {
+        if(n==0)
+        {
+            a[j] = a[j] - 1;
+            a[j+1] = 90;
+            break;
+        }
+        else
+            a[j++] = n/pow(26,i) + 64;
+        n %= (int)pow(26,i--);
+        printf("%d ",n);
+    }
+    a[j]='\0';
+    printf("%s",a);
     return 0;
 }
