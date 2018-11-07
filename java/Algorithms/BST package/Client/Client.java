cd691b02eb8b436aa219c6a7b834188a2d74b915
shubhamguptaji <shubham2604gupta@gmail.com>
2018-11-07 22:40:41 +0530

    Tested for 1 million lines

--- java/Algorithms/BST package/Client/Client.java
+++ java/Algorithms/BST package/Client/Client.java
@@ -1,5 +1,5 @@
 package Client;
- import BST.*;
+import BST.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,14 +7,14 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("C:\\Users\\Shubham\\Desktop\\New folder\\Client\\random.txt");
+        File file = new File("C:\\Users\\Shubham\\Desktop\\New folder\\Client\\test.txt");
         Scanner sc = new Scanner(file);
         BinarySearchTree tree = new BinarySearchTree();
         while(sc.hasNextLine()) {
             String str = sc.nextLine();
             String[] arr = str.split(" ");
             for(int i=0;i<arr.length;i++) {
-                if(arr[i].length() >= 7) {
+                if(arr[i].length() >= 10) {
                     tree.insert(arr[i]);
                 }
             }
