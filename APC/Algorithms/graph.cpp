a56fde46e9b012629ae40276e2265b93af9632a2
shubhamguptaji <shubham2604gupta@gmail.com>
2018-11-24 00:04:37 +0530

    dfs traversal in graph

--- APC/Algorithms/graph.cpp
+++ APC/Algorithms/graph.cpp
@@ -11,6 +11,7 @@ void insert(int a, int b) {
 void bsf(int a) {
     queue<int> q;
     q.push(a);
+    vis[a] = 1;
     while(!q.empty()) {
         int temp = q.front();
         cout<<temp<< " ";
@@ -22,6 +23,37 @@ void bsf(int a) {
             q.push(v[temp][i]);
             }
             // cout<<v[temp][i];
+        }
+    }
+}
+// recursive dfs
+void dfs(int a) {
+    vis[a] = 1;
+    cout<<a<<" ";
+    for(int i=0;i<v[a].size();i++) {
+        if(vis[v[a][i]]==0)
+         {
+            //  cout<<v[a][i]<<" ";
+            dfs(v[a][i]);
+         }
+    }
+}
+
+//iterative
+
+void iterativeDFS(int a) {
+    stack<int> stk;
+    stk.push(a);
+    vis[a]=1;
+    while(!stk.empty()) {
+        int temp = stk.top();
+        stk.pop();
+        cout<<temp<<" ";
+        for(int i=0;i<v[temp].size();i++) {
+            if(vis[v[temp][i]]==0) {
+                vis[v[temp][i]] = 1;
+                stk.push(v[temp][i]);
+            }
         }
     }
 }
@@ -37,5 +69,7 @@ int main() {
     insert(6,8);
     insert(7,9);
     insert(3,2);
-    bsf(0);
+    insert(5,0);
+    // bsf(0);
+    iterativeDFS(0);
 }
