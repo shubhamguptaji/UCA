0e19900e0a1b3481f89281c647ac761eb2d6e92f
shubhamguptaji <shubham2604gupta@gmail.com>
2018-12-03 21:14:03 +0530

    DP

---
+++ ICPC/GRID.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        char *a[n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> a[i][j];
+        int count = 0;
+        cout << count << endl;
+    }
+    return 0;
+}
