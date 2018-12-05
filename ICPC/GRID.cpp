50a19d2bff75208b860a7048197e5ec6cae30c37
shubhamguptaji <shubham2604gupta@gmail.com>
2018-12-05 14:30:48 +0530

    sherlock and the grid problem codechef

--- ICPC/GRID.cpp
+++ ICPC/GRID.cpp
@@ -9,11 +9,31 @@ int main()
     {
         int n;
         cin >> n;
-        char *a[n];
+        char a[n][n];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 cin >> a[i][j];
         int count = 0;
+        int right[n][n];
+        int bottom[n][n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                if (a[i][j] == '.')
+                    right[i][j] = (j != n - 1) ? right[i][j + 1] : 1;
+                else
+                    right[i][j] = 0;
+                if (a[j][i] == '.')
+                    bottom[j][i] = (j != n - 1) ? bottom[j + 1][i] : 1;
+                else
+                    bottom[j][i] = 0;
+            }
+        }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (right[i][j] == 1 && bottom[i][j] == 1)
+                    count++;
         cout << count << endl;
     }
     return 0;
